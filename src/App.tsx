37a39c5e49df8f101da398acdcf8dd2490fbc490
Marcelo Riios Almanza <marcelo10rios@hotmail.com>
2022-06-26 17:59:09 -0500

    greeting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 
+import { useUser } from "./hooks/users/useUser";
 import HomePage from "./pages/Home/HomePage";
 import NavBar from "./components/NavBar/NavBar";
 import "./App.css";
@@ -10,13 +11,31 @@ const commonRouteProps = {
 };
 
 const App = () => {
+  // Using this to get a random user, because we don't have a logged in user to get the data from
+  // Could use context to share the user data
+  const {
+    loading: singleUserLoading,
+    data: singleUserData,
+    called: singleUserCalled
+  } = useUser("cl4oxtzns6w8f4j108g5l92ia");
+  let greetingText = "Welcome!",
+    currentUser;
+
+  if (!singleUserLoading && singleUserCalled) {
+    currentUser = singleUserData.User;
+    greetingText = `Hello, ${currentUser.firstName} ${currentUser.lastName}!`;
+  }
+
   return (
     <div className="App">
       <NavBar />
       <h1>Post Analytics Dashboard</h1>
-      <Routes>
-        <Route {...commonRouteProps} path="/" element={<HomePage />} />
-      </Routes>
+      <div className="App__Content">
+        <h2>{greetingText}</h2>
+        <Routes>
+          <Route {...commonRouteProps} path="/" element={<HomePage />} />
+        </Routes>
+      </div>
     </div>
   );
 };
